e1f0adae9cb34609242094492ea425249bcb13b1
ValentinaMarrazzo <valentina.marrazzo.99@outlook.it>
2022-04-11 14:24:35 +0200

    esercizio js

---
+++ m1/w3/d1/js/script.js
@@ -0,0 +1,56 @@
+/* 
+In javascript si lavora con 5 tipi di dato:
+- numero (es: 5)
+- stringa (es: '5')
+- booleano (es: true/false)
+- oggetto (liste di dati e funzioni separate per nome. Coppia nome-valore)
+- array (lista di valori identificati per numero).
+
+I dati sono assegnati a dei contenitori, cioè le variabili (var = dato;).
+
+*/
+
+var num = 5;
+var string = 'testo';
+var boolean = true;
+var obj = {
+    nome: 'Nome',
+    cognome: 'Cognome',
+    eta: 22
+};
+var array = [
+    1,
+    2,
+    3,
+    'stella'
+];
+
+/* Per mostrare le variabili e gli oggetti all'interno del documento può essere usato document.write(variabile/oggetto). Tuttavia è un sistema antiquato che non andrebbe usato, perché ad un nuovo caricamento cancella quello che è presente nella pagina per mostrare ciò che deve mostrare. */
+
+document.write(obj.nome);
+
+/*operatori e funzioni */
+
+function somma() {
+    document.write(2 + 6);
+}
+
+function ottieniSomma() {
+    return 2 + 6;
+}
+
+/*La differenza tra funzioni con return e senza è: le funzioni con return non hanno un dato, ma se lo devono ricavare al momento del lancio della funzione. Si può richiamare una funzione per eseguirla assegnandola ad una variabile e richiamando poi la variabile. */
+
+/*è possibile richiamare una funzione prima che venga caricata tramite il meccanismo di hoisting. */
+
+/*lavorare con gli oggetti del DOM */
+
+var div1 = document.getElementById('elem');
+div1.innerHTML = 'Così cambio il valore all interno dell elemento'; //non fa il parse dell'html
+//div1.style.backgroundColor = 'red';
+
+div1.classList.add('') //aggiunge una classe ad un elemento ma solo se ce ne sono già delle altre
+
+var blocchi = document.getElementsByClassName('blocco');//getElementsByClassName è un array di oggetti
+
+blocchi[0].style.backgroundColor = 'red'; //è obbligatorio specificare quale elemento all'interno dell'array si richiama
