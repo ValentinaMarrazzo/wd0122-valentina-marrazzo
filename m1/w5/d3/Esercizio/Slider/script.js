4928493da4b3446b92b0692ca2e0e33d66094e01
ValentinaMarrazzo <valentina.marrazzo.99@outlook.it>
2022-04-28 18:03:30 +0200

    esercizio slider

--- m1/w5/d3/Esercizio/Slider/script.js
+++ m1/w5/d3/Esercizio/Slider/script.js
@@ -8,28 +8,28 @@ let face = document.querySelectorAll('.face')
 //console.log(face)
 switch(countL) {
     case 1:
-        face[0].style.transform = 'rotateY(90deg) translateZ(16vh)'
-        face[1].style.transform = 'rotateY(180deg) translateZ(16vh)'
-        face[2].style.transform = 'rotateY(0deg) translateZ(16vh)'
-        face[3].style.transform = 'rotateY(-90deg) translateZ(16vh)'
+        face[0].style.transform = 'rotateY(90deg) translateZ(15vh)'
+        face[1].style.transform = 'rotateY(180deg) translateZ(15vh)'
+        face[2].style.transform = 'rotateY(0deg) translateZ(15vh)'
+        face[3].style.transform = 'rotateY(-90deg) translateZ(15vh)'
         break;
     case 2:
-        face[0].style.transform = 'rotateY(0deg) translateZ(16vh)'
-        face[1].style.transform = 'rotateY(90deg) translateZ(16vh)'
-        face[2].style.transform = 'rotateY(-90deg) translateZ(16vh)'
-        face[3].style.transform = 'rotateY(180deg) translateZ(16vh)'
+        face[0].style.transform = 'rotateY(0deg) translateZ(15vh)'
+        face[1].style.transform = 'rotateY(90deg) translateZ(15vh)'
+        face[2].style.transform = 'rotateY(-90deg) translateZ(15vh)'
+        face[3].style.transform = 'rotateY(180deg) translateZ(15vh)'
         break;
     case 3:
-        face[0].style.transform = 'rotateY(-90deg) translateZ(16vh)'
-        face[1].style.transform = 'rotateY(0deg) translateZ(16vh)'
-        face[2].style.transform = 'rotateY(180deg) translateZ(16vh)'
-        face[3].style.transform = 'rotateY(90deg) translateZ(16vh)'
+        face[0].style.transform = 'rotateY(-90deg) translateZ(15vh)'
+        face[1].style.transform = 'rotateY(0deg) translateZ(15vh)'
+        face[2].style.transform = 'rotateY(180deg) translateZ(15vh)'
+        face[3].style.transform = 'rotateY(90deg) translateZ(15vh)'
         break;
     case 4:
-        face[0].style.transform = 'rotateY(180deg) translateZ(16vh)'
-        face[1].style.transform = 'rotateY(-90deg) translateZ(16vh)'
-        face[2].style.transform = 'rotateY(90deg) translateZ(16vh)'
-        face[3].style.transform = 'rotateY(0deg) translateZ(16vh)'
+        face[0].style.transform = 'rotateY(180deg) translateZ(15vh)'
+        face[1].style.transform = 'rotateY(-90deg) translateZ(15vh)'
+        face[2].style.transform = 'rotateY(90deg) translateZ(15vh)'
+        face[3].style.transform = 'rotateY(0deg) translateZ(15vh)'
         break;
     default:
         countL = 0;
@@ -44,28 +44,28 @@ function rotateRight() {
 let face = document.querySelectorAll('.face')
 switch(countR) {
     case 1:
-        face[0].style.transform = 'rotateY(-90deg) translateZ(16vh)'
-        face[1].style.transform = 'rotateY(0deg) translateZ(16vh)'
-        face[2].style.transform = 'rotateY(180deg) translateZ(16vh)'
-        face[3].style.transform = 'rotateY(90deg) translateZ(16vh)'
+        face[0].style.transform = 'rotateY(-90deg) translateZ(15vh)'
+        face[1].style.transform = 'rotateY(0deg) translateZ(15vh)'
+        face[2].style.transform = 'rotateY(180deg) translateZ(15vh)'
+        face[3].style.transform = 'rotateY(90deg) translateZ(15vh)'
         break;
     case 2:
-        face[0].style.transform = 'rotateY(0deg) translateZ(16vh)'
-        face[1].style.transform = 'rotateY(90deg) translateZ(16vh)'
-        face[2].style.transform = 'rotateY(-90deg) translateZ(16vh)'
-        face[3].style.transform = 'rotateY(180deg) translateZ(16vh)'
+        face[0].style.transform = 'rotateY(0deg) translateZ(15vh)'
+        face[1].style.transform = 'rotateY(90deg) translateZ(15vh)'
+        face[2].style.transform = 'rotateY(-90deg) translateZ(15vh)'
+        face[3].style.transform = 'rotateY(180deg) translateZ(15vh)'
         break;
     case 3:
-        face[0].style.transform = 'rotateY(90deg) translateZ(16vh)'
-        face[1].style.transform = 'rotateY(180deg) translateZ(16vh)'
-        face[2].style.transform = 'rotateY(0deg) translateZ(16vh)'
-        face[3].style.transform = 'rotateY(-90deg) translateZ(16vh)'
+        face[0].style.transform = 'rotateY(90deg) translateZ(15vh)'
+        face[1].style.transform = 'rotateY(180deg) translateZ(15vh)'
+        face[2].style.transform = 'rotateY(0deg) translateZ(15vh)'
+        face[3].style.transform = 'rotateY(-90deg) translateZ(15vh)'
         break;
     case 4:
-        face[0].style.transform = 'rotateY(180deg) translateZ(16vh)'
-        face[1].style.transform = 'rotateY(-90deg) translateZ(16vh)'
-        face[2].style.transform = 'rotateY(90deg) translateZ(16vh)'
-        face[3].style.transform = 'rotateY(0deg) translateZ(16vh)'
+        face[0].style.transform = 'rotateY(180deg) translateZ(15vh)'
+        face[1].style.transform = 'rotateY(-90deg) translateZ(15vh)'
+        face[2].style.transform = 'rotateY(90deg) translateZ(15vh)'
+        face[3].style.transform = 'rotateY(0deg) translateZ(15vh)'
         break;
     default:
         countR = 0;
